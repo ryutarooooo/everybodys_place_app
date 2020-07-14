9a83b5d72c86a74cac098207cba7877fb6f571d7
ryutaro <ryutaro@icloud.com>
2020-07-14 14:07:16 +0900

    初期データを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-Comment.create!(user_id: 3, tweet_id: 2, title: "こんばんは", content: "おはよう")
-Comment.create!(user_id: 1, tweet_id: 3, title: "朝になりました", content: "こんにちは")
-Comment.create!(user_id: 2, tweet_id: 1, title: "夜になりました", content: "こんばんは")
-Comment.create!(user_id: 1, tweet_id: 2, title: "今日は勉強します", content: "テスト")
-Comment.create!(user_id: 2, tweet_id: 3, title: "もう限界です", content: "おやすみ")
+# user1 = User.create!(name: "永田", profile_image: "写真", age: 24, place: "愛知県", content: "よろしくお願いします", career: "愛知高校", using_reason: "子供の為に", insta_id: "112233", email: "a@a", password: "aaaaaa")
 
-puts "成功しました"
+# user2 = User.create!(name: "遠藤", profile_image: "写真", age: 25, place: "東京", content: "初めまして", career: "東京高校", using_reason: "妹の為に", insta_id: "111111", email: "b@b", password: "bbbbbb")
+
+# user3 = User.create!(name: "たけ", profile_image: "写真", age: 38, place: "大阪県", content: "皆さんこんにちは", career: "大阪高校", using_reason: "親の為に", insta_id: "222222", email: "c@c", password: "cccccc")
+
+# tweet1 = Tweet.create(user_id: 1, title: "こんにちは", content: "6時に起きました", image: "太陽")
+# tweet2 = Tweet.create(user_id: 2, title: "おやすみなさい", content: "23時に寝ました", image: "月")
+# tweet3 = Tweet.create(user_id: 3, title: "お腹すいた", content: "12時に昼飯を食べました", image: "ラーメン")
+# tweet4 = Tweet.create(user_id: 3, title: "出かけました", content: "海に行きました", image: "海")
+# tweet5 = Tweet.create(user_id: 3, title: "運動しました", content: "サッカーやりました", image: "サッカーボール")
+
+# favorite1 = Favorite.create!(user_id: 1, tweet_id: 3)
+
+# favorite2 = Favorite.create!(user_id: 3, tweet_id: 1)
+
+# favorite3 = Favorite.create!(user_id: 2, tweet_id: 5)
+
+# favorite4 = Favorite.create!(user_id: 2, tweet_id: 3)
+
+# favorite5 = Favorite.create!(user_id: 1, tweet_id: 2)
+
+# puts "成功しました"
