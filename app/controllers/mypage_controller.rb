7d18132021080d0fa2779f09d48a8564fa6a9ef6
ryutaro <ryutaro@icloud.com>
2020-07-15 21:02:53 +0900

    mypageルーディング対応

---
+++ app/controllers/mypage_controller.rb
@@ -0,0 +1,17 @@
+class MypageController < ApplicationController
+  # before_action :correct_user, only: %i[edit show update]
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  # def correct_user
+  #   @User = current_user.User.find_by(id: params[:id])
+  #   redirect_to root_path if @message.nil?
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "homes#index"
   devise_for :users
-  # resources :users, only: %i[] do
-  #   resource :mypages, only: %i[]
-  # end
+  resources :users, only: %i[index show]
+  resource :mypage, only: %i[show edit update], controller: "mypage"
 end
