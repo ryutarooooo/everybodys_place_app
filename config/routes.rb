234a365e2e71ded3e6b062a54c80bac7b0406690
ryutaro <ryutaro@icloud.com>
2020-07-15 13:31:11 +0900

    Userページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "homes#index"
-  resources :users
+  devise_for :users
+  # resources :users, only: %i[] do
+  #   resource :mypages, only: %i[]
+  # end
 end
