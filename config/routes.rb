7a5ada477f4ea1d77ca19c742361006f1ae32e85
ryutaro <ryutaro@icloud.com>
2020-07-16 17:38:13 +0900

    tweetsのviewとコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[index show]
   resource :mypage, only: %i[show edit update], controller: "mypage"
+  resource : tweets
 end
