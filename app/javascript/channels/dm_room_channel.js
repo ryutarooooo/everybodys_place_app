ffce86a398c0207ad72dd0d5ca638c36c01b51e6
ryutaro <ryutaro@icloud.com>
2020-08-01 14:30:51 +0900

    DMの送信をエンターからボタンに変更

--- app/javascript/channels/dm_room_channel.js
+++ app/javascript/channels/dm_room_channel.js
@@ -1,34 +1,45 @@
 import consumer from "./consumer"
 
 $(function () {
-  const dmChannel = consumer.subscriptions.create({
-    channel: 'DmRoomChannel',
-    receive_user_id: $('#dms').data('receive_user_id')
-  }, {
-    connected() {
-      // Called when the subscription is ready for use on the server
-    },
+  const dmArea = document.getElementById("dm-area")
+  if (dmArea) {
+    const dmChannel = consumer.subscriptions.create({
+      channel: 'DmRoomChannel',
+      receive_user_id: $('#dms').data('receive_user_id')
+    }, {
+      connected() {
+        // Called when the subscription is ready for use on the server
+      },
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-    received: function (data) {
-      return $('#dms').append(data['dm']);
-    },
+      received: function (data) {
+        return $('#dms').append(data['dm']);
+      },
 
-    speak: function (dm) {
-      return this.perform('speak', {
-        dm: dm
-      });
-    }
-  });
+      speak: function (dm) {
+        return this.perform('speak', {
+          dm: dm
+        });
+      }
+    });
 
-  $(document).on('keypress', '[data-behavior~=dm_room_speaker]', function (event) {
-    if (event.keyCode === 13) {
-      dmChannel.speak(event.target.value);
-      event.target.value = '';
-      return event.preventDefault();
-    }
-  });
+    //   $(document).on('keypress', '[data-behavior~=dm_room_speaker]', function (event) {
+    //     if (event.keyCode === 13) {
+    //       dmChannel.speak(event.target.value);
+    //       event.target.value = '';
+    //       return event.preventDefault();
+    //     }
+    //   });
+    // });
+
+    const dmButton = document.getElementById("dm-button")
+    dmButton.addEventListener("click", function () {
+      let content = dmArea.value
+      dmChannel.speak(content)
+      dmArea.value = ''
+    })
+  }
 });
