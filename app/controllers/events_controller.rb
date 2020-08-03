6d0fbac7ee7f1bad431bb0ea5f70b849b71e4fb3
ryutaro <ryutaro@icloud.com>
2020-08-03 16:41:18 +0900

    イベントの編集、削除機能実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,12 +24,19 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    event = Event.find(params[:id])
+    event.update!(event_params)
+    redirect_to event
   end
 
   def destroy
+    event = Event.find(params[:id])
+    event.destroy!
+    redirect_to event
   end
 
   private
